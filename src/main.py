f4aa5ee32a0d5f8d1ca585fcd438e817a346077a
Tommaso <tommaso131195@gmail.com>
2019-06-02 12:35:04 +0200

    Added a full trained model

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import genetic_algorithm as ga
 
 def main():
 
-	number_of_gen = 25
+	number_of_gen = 100
 	snakes_per_gen = 100
 	shape = (5, 31, 11, 3)
 
@@ -24,21 +24,29 @@ def main():
 				G.play()
 
 		result = np.mean([sn.fitness for sn in generation])
-		print("generation", gen+1, ": ", result)
+		print("generation", gen+1, ":", result)
 
 		generation = ga.create_generation(generation)
 		
-	save(generation)
-
-	generation = load()
-	generation = sort_generation(generation)
-
-	G = game()
-	G = game([10, 20], True, 1000)
-	G.add_snake(generation[0])
+	for sn in generation:
+		G = game([10, 20], False, 100)
+		G.add_snake(sn)
 
 	while G.snake.is_alive:
 		G.play()
+
+	# generation = sort_generation(generation)
+	save(generation)
+
+	# generation = load()
+	# generation = sort_generation(generation)
+
+	# G = game()
+	# G = game([10, 20], True, 1000)
+	# G.add_snake(generation[0])
+
+	# while G.snake.is_alive:
+	# 	G.play()
 
 
 if __name__ == "__main__":
